b3ae714d209fa55c6c34107b0c01c9809fad2f3b
TungLe <tunglt@segu.vn>
2020-11-18 13:11:04 +0700

    - open url

--- src/main/java/com/celano/Auto.java
+++ src/main/java/com/celano/Auto.java
@@ -1,7 +1,24 @@
 package com.celano;
 
+import com.celano.config.DriverResourceLocator;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
 public class Auto {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", new DriverResourceLocator().getChromeDriverLocation());
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-notifications");
+//        options.addArguments("disable-infobars");
 
+        WebDriver driver = new ChromeDriver(options);
+        Actions actions = new Actions(driver);
+
+//        driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(1080, 750));
+        driver.get("https://www.facebook.com/");
     }
 }
